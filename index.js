f5fd7a99331a2d530cc328e645bf19e24c1871b3
Ignacio Yz <ignacioyz@gmail.com>
2022-03-14 14:22:05 -0400

    commit tree 1e9ccdc9f7816571adb9300f2cd6251bd83409ee

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Follow along with the examples here
+function sayHelloGuadalupe() {
+    console.log("Hello Guadalupe!");
+  }
+  sayHelloGuadalupe();
+
+function sayHelloLiz() {
+    console.log("Hello Liz!");
+  }
+  sayHelloLiz();
+
+function sayHelloSamip() {
+    console.log("Hello Samip!");
+  }
+  sayHelloSamip();
+
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Julio", "Hello");
